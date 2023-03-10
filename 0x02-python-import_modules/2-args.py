e679aee2661d1edb68140dd50417975e00a956e9
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-03-10 23:54:35 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
-    else if count == 1:
+    elif count == 1:
         print(f"1 argument:\n1: {argv[1]}")
     else:
-        print(f"{count} arguments:\n")
+        print(f"{count} arguments:")
         for i in range(count):
             print(f"{i + 1}: {argv[i + 1]}")
