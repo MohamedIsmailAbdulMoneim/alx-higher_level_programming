09c6f5f86b35e6ea13052ffb34a5a887cf78a060
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-03-10 23:42:41 +0200

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ = "__main__":
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    else if count == 1:
+        print(f"1 argument:\n1: {argv[1]}")
+    else:
+        print(f"{count} arguments:\n")
+        for i in range(count):
+            print(f"{i + 1}: {argv[i + 1]}")
