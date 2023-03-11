b0323cc9c790867d7cb5f7895327e7b381ff4bb0
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-03-11 21:49:16 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """ Add all arguments."""
-    import sys
-
-    result = 0
-
-    for i in range(len(sys.argv) - 1):
-        result += (int(sys.argv[i + 1]))
-    print("{:d}".format(result))
+add = 0
+for s in argv[1:]:
+    add += int(s)
+print("{:d}".format(add))
