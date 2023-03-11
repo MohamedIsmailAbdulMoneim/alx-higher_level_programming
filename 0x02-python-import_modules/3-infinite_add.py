9caabdd0302a53c0b3d7fc9e9da14cf389e3aa21
Mohamed Ismail Abdul-Monein <70236712+MohamedIsmailAbdulMoneim@users.noreply.github.com>
2023-03-11 21:38:56 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     sum = 0
 
     for i in range(count):
-        if argv[i + 1].isdigit():
-            sum += int(argv[i + 1])
+        sum += int(argv[i + 1])
     print(sum)
